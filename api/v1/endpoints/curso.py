4e6bf5a7fe8718354d26afe2b3093bf241cca81c
Erick Bessa <erick.bessa96@gmail.com>
2023-01-24 16:13:07 -0300

    create and import routers of cursos

--- api/v1/endpoints/curso.py
+++ api/v1/endpoints/curso.py
@@ -5,6 +5,7 @@ from fastapi import status
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import Response
+from pydantic.networks import HttpUrl
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -27,7 +28,65 @@ async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)
 async def get_cursos(db = Depends(get_session())):
     async with db as session:
         query = select(CursoModel)
+        print(f'POST_query: {query}')
         result = await session.execute(query)
         cursos: List[CursoModel] = result.scalars().all()
 
         return cursos
+
+@router.get('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        print(f'GET_query: {query}')
+        result = await session.execute(query)
+        curso = result.scalar_one_or_none()
+
+        if curso:
+            return curso
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                detail=f"Não foi possível encontrar curso com o id:{curso_id}")
+
+
+@router.put('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_202_ACCEPTED)
+async def put_curso(curso_id: int, curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        print(f'PUT_query: {query}')
+        result = await session.execute(query)
+        curso_up = result.scalar_one_or_none()
+
+        if curso_up:
+            curso_up.titulo = curso.titulo
+            curso_up.aulas = curso.aulas
+            curso_up.horas = curso.horas
+
+            await session.commit()
+            
+            return curso_up
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                detail=f"Não foi possível encontrar curso com o id:{curso_id}")
+
+
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        print(f'DELETE_query: {query}')
+        result = await session.execute(query)
+        curso_del = result.scalar_one_or_none()
+
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+            
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                detail=f"Não foi possível encontrar curso com o id:{curso_id}")
